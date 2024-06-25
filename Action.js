b6feef6c404e6ec4e0c330ccf998a8de7a7e70cf
Ujjawal Kumar <ujjawalkr67@gmail.com>
2024-06-25 13:47:49 +0530

    backend

---
+++ Action.js
@@ -0,0 +1,10 @@
+const ACTIONS={
+    JOIN:'join',
+    JOINED:'joined',
+    DISCONNECT:'disconnected',
+    CODE_CHANGE:'code-change',
+    SYNC_CODE:'sync-code',
+    LEAVE:'leave',
+    SEND_MESSAGE: "send-message",
+};
+module.exports =ACTIONS;
